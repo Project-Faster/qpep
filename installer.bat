e8daed637c1a9847054d7d201fea445c7a03ec9e
Vittorio Parrella <vittorioparrella+1@gmail.com>
2025-03-26 23:45:28 +0100

    feat: drop x86 platform support fix: directory permissions test fix: versioinfo inclusion in ci build

--- installer.bat
+++ installer.bat
@@ -7,38 +7,8 @@ echo **** QPEP INSTALLER BUILDER ****
 echo ********************************
 echo.
 
-set /A BUILD64=0
-set /A BUILD32=0
-
-if "%1" EQU "--build32" (
-    set /A BUILD32=1
-)
-if "%1" EQU "--build64" (
-    set /A BUILD64=1
-)
-if "%2" EQU "--build32" (
-    set /A BUILD32=1
-)
-if "%2" EQU "--build64" (
-    set /A BUILD64=1
-)
-if "%2" EQU "--rebuild" (
+if "%1" EQU "--rebuild" (
     goto installer
-)
-
-if %BUILD64% EQU 0 (
-    if %BUILD32% EQU 0 (
-        echo Usage: installer.bat [--build32] [--build64]
-        echo.
-        goto fail
-    )
-)
-if %BUILD32% EQU 0 (
-    if %BUILD64% EQU 0 (
-        echo Usage: installer.bat [--build32] [--build64]
-        echo.
-        goto fail
-    )
 )
 
 ECHO [Requirements check: GO]
@@ -55,8 +25,6 @@ if %ERRORLEVEL% GEQ 1 goto fail
 
 ECHO [Cleanup]
 DEL /S /Q build 2> NUL
-RMDIR build\64bits 2> NUL
-RMDIR build\32bits 2> NUL
 RMDIR build\installer 2> NUL
 RMDIR build 2> NUL
 
@@ -64,10 +32,6 @@ echo OK
 
 ECHO [Preparation]
 MKDIR build\  2> NUL
-if %ERRORLEVEL% GEQ 1 goto fail
-MKDIR build\64bits\ 2> NUL
-if %ERRORLEVEL% GEQ 1 goto fail
-MKDIR build\32bits\ 2> NUL
 if %ERRORLEVEL% GEQ 1 goto fail
 
 go clean
@@ -81,72 +45,32 @@ if %ERRORLEVEL% GEQ 1 goto fail
 echo OK
 
 set GOOS=windows
-if %BUILD64% NEQ 0 (
-    set GOARCH=amd64
-    go clean -cache
+set GOARCH=amd64
+go clean -cache
 
-    ECHO [Copy dependencies 64bits]
-    COPY /Y windivert\x64\* build\64bits\
-    if %ERRORLEVEL% GEQ 1 goto fail
-    echo OK
+ECHO [Copy dependencies 64bits]
+COPY /Y windivert\x64\* build\
+if %ERRORLEVEL% GEQ 1 goto fail
+echo OK
 
-    ECHO [Build 64bits server/client]
-    set CGO_ENABLED=1
-    ECHO [Build backends 64bits]
-    pushd backend
-    go generate
-    if %ERRORLEVEL% GEQ 1 goto fail
-    popd
+ECHO [Build server/client]
+set CGO_ENABLED=1
 
-    ECHO [Build 64bits server/client]
-    go build -o build\64bits\qpep.exe
-    if %ERRORLEVEL% GEQ 1 goto fail
+go build -o build\qpep.exe
+if %ERRORLEVEL% GEQ 1 goto fail
 
-    echo OK
+echo OK
 
-    ECHO [Build 64bits tray icon]
-    pushd qpep-tray
-    if %ERRORLEVEL% GEQ 1 goto fail
-    set CGO_ENABLED=0
-    go build -ldflags -H=windowsgui -o ..\build\64bits\qpep-tray.exe
-    if %ERRORLEVEL% GEQ 1 goto fail
-    popd
+ECHO [Build tray icon]
+pushd qpep-tray
+if %ERRORLEVEL% GEQ 1 goto fail
 
-    echo OK
-)
+set CGO_ENABLED=0
+go build -ldflags -H=windowsgui -o ..\build\qpep-tray.exe
+if %ERRORLEVEL% GEQ 1 goto fail
+popd
 
-if %BUILD32% NEQ 0 (
-    set GOARCH=386
-    go clean -cache
-
-    ECHO [Copy dependencies 32bits]
-    COPY /Y windivert\x86\* build\32bits\
-    if %ERRORLEVEL% GEQ 1 goto fail
-    echo OK
-
-    set CGO_ENABLED=1
-    ECHO [Build backends 32bits]
-    pushd backend
-    go generate
-    if %ERRORLEVEL% GEQ 1 goto fail
-    popd
-
-    ECHO [Build 32bits server/client]
-    go build -x -o build\32bits\qpep.exe
-    if %ERRORLEVEL% GEQ 1 goto fail
-
-    echo OK
-
-    ECHO [Build 32bits tray icon]
-    pushd qpep-tray
-    if %ERRORLEVEL% GEQ 1 goto fail
-    set CGO_ENABLED=0
-    go build -ldflags -H=windowsgui -o ..\build\32bits\qpep-tray.exe
-    if %ERRORLEVEL% GEQ 1 goto fail
-    popd
-
-    echo OK
-)
+echo OK
 
 :installer
 echo [Build of installer]
